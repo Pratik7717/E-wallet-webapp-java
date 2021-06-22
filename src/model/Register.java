929bf3e3f4e11c81565fc740677c606c7e390db1
Pratik Jagade <pratik.21810767@viit.ac.in>
2021-06-22 12:51:41 +0530

    Register model class added

---
+++ src/model/Register.java
@@ -0,0 +1,66 @@
+package model;
+
+public class Register {
+	private String fname,lname,mobile,email,password,code;
+
+	public Register(String fname, String lname, String mobile, String email, String password, String code) {
+		super();
+		this.fname = fname;
+		this.lname = lname;
+		this.mobile = mobile;
+		this.email = email;
+		this.password = password;
+		this.code=code;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+	
+	
+}
