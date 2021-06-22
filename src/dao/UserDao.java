547b92ac2c98b5f3d719eef0142486c36caa98c3
Pratik Jagade <pratik.21810767@viit.ac.in>
2021-06-22 14:08:59 +0530

    Added validateuser method in userdao

--- src/dao/UserDao.java
+++ src/dao/UserDao.java
@@ -3,6 +3,7 @@ package dao;
 import java.sql.*;
 
 import model.Register;
+import model.User;
 
 public class UserDao {
 	private int i=0;
@@ -23,6 +24,26 @@ public class UserDao {
 		}
 		
 		return i;
+	}
+	
+	public User validateUser(String email, String password) {
+		User user=null;
+		try {
+			Connection con=MyConnection.getConnection();
+			PreparedStatement ps=con.prepareStatement("select * from user where email=? AND password=?");
+			ps.setString(1, email);
+			ps.setString(2, password);
+			ResultSet rs=ps.executeQuery();
+			if(rs.next()) {
+				user= new User(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4),rs.getString(5), rs.getString(6), rs.getDouble(7));
+			}
+		} catch (SQLException e) {
+			System.out.println("Exception in ValidateUser() dao");
+			e.printStackTrace();
+		}
+		
+		
+		return user;
 	}
 	
 }
